f21ce672ae0f43d8c4fd3efbf73be49e221a8ff0
Syndric <65517819+Syndric@users.noreply.github.com>
2025-02-13 22:09:53 -0500

    fix: edit example, base program

---
+++ example.ino
@@ -0,0 +1,60 @@
+/*
+  ===== DEPENDENCIES =====
+*/
+
+// #include "RGBHandler.h"
+// #include "LineHandler.h"
+// #include "SelectorHandler.h"
+// #include "TOFHandler.h"
+// #include "MPUHandler.h"
+#include "DriveHandler.h"
+#include <SoftwareSerial.h>
+#include "RFHandler.h"
+
+/* --- Pinout ---
+ 0 - xxx
+*/
+
+/*
+  ===== DEFINITIONS =====
+*/
+
+// --- Constants ---
+// EX: #define PWM_LOW 980
+
+// --- Input Pins ---
+// EX: const int kSelector[2] = {11, 12};
+
+// --- Output Pins ---
+// EX: const int kRGB[3] = {8, 7, 4};               // RGB Pins
+
+/*
+  ===== SETUP =====
+*/
+
+// --- Input Handlers ---
+// EX: RFHandler rf(kRF[0], kRF[1]);
+
+// --- Output Handlers ---
+// EX: DriveHandler drive(kMotors[0][0], kMotors[0][1], kMotors[1][0], kMotors[1][1], true, false);
+
+void setup()
+{
+  Serial.begin(115200); // Begin serial
+
+  // --- Begin Handlers ---
+  // EX: drive.begin();
+}
+
+void loop()
+{
+  // --- Input Handling ---
+  rf.Receive();
+
+  // --- Input Parsing
+  rf.Parse(true);
+  drive.Set(rf.ly, rf.rx, rf.b1, rf.b2);
+
+  // -- Output ---
+  drive.Update();
+}
