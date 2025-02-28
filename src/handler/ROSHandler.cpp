ee43272e046de27ee69a3f834ebae071694eef21
Syndric <65517819+Syndric@users.noreply.github.com>
2025-02-28 11:19:53 -0500

    add: Ros handler

---
+++ src/handler/ROSHandler.cpp
@@ -0,0 +1,30 @@
+#include "ROSHandler.h"
+
+ROSHandler::ROSHandler() {}
+
+void ROSHandler::Setup()
+{
+    nh.initNode();
+    broadcaster.init(nh);
+}
+
+void ROSHandler::Update(const Pose2D &pose)
+{
+    static elapsedMillis timer = 0;
+    if (timer > minDelay)
+    {
+        timer = 0;
+        t.header.frame_id = odom;
+        t.child_frame_id = base_link;
+        t.transform.translation.x = pose.x;
+        t.transform.translation.y = pose.y;
+        t.transform.translation.z = 0;
+        t.transform.rotation.x = 0.0;
+        t.transform.rotation.y = 0.0;
+        t.transform.rotation.z = pose.theta;
+        t.transform.rotation.w = 1.0;
+        t.header.stamp = nh.now();
+        broadcaster.sendTransform(t);
+        nh.spinOnce();
+    }
+}
---
+++ src/handler/ROSHandler.h
@@ -0,0 +1,25 @@
+#ifndef ROSHANDLER_H
+#define ROSHANDLER_H
+
+#include <ros.h>
+#include <ros/time.h>
+#include <tf/transform_broadcaster.h>
+#include <elapsedMillis.h>
+#include "../drive/math/Pose2D.h"
+class ROSHandler
+{
+public:
+    ROSHandler();
+    void Setup();
+    void Update(const Pose2D &pose);
+
+private:
+    ros::NodeHandle nh;
+    geometry_msgs::TransformStamped t;
+    tf::TransformBroadcaster broadcaster;
+    unsigned long minDelay = 10; // ms
+    char base_link[11] = "/base_link";
+    char odom[6] = "/odom";
+};
+
+#endif
