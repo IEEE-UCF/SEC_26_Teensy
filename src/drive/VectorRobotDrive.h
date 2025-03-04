fa98a8438990188250958dda1234d446d18deb14
BigManRaffa <animedudebro@gmail.com>
2025-03-03 19:21:19 -0500

    VectorRobotDrive.h update

--- src/drive/VectorRobotDrive.h
+++ src/drive/VectorRobotDrive.h
@@ -4,15 +4,13 @@
 #include "SimpleRobotDrive.h"
 #include "MOTORCONFIG.h"
 
-class VectorRobotDrive : public SimpleRobotDrive
-{
+class VectorRobotDrive : public SimpleRobotDrive {
 public:
-    VectorRobotDrive(const MotorSetup motorSetups[], int numMotors);
+    VectorRobotDrive(const MotorSetup motorSetups[], int numMotors, Print &output);
     void Set(const Pose2D &speedPose);
-    Pose2D GetVelocity();
+    Pose2D GetVelocity() const;
 
 private:
     Pose2D speedPose;
 };
-
 #endif
