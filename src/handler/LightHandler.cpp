e3aa0634729e5f2762e9d2ad229ba789d9f2ac5c
Syndric <65517819+Syndric@users.noreply.github.com>
2025-03-17 11:44:24 -0400

    fix: lighthandler wire1

---
+++ src/handler/LightHandler.cpp
@@ -0,0 +1,50 @@
+#include "LightHandler.h"
+
+LightHandler::LightHandler(int cLight) : cLight(cLight), lightMeter(), lightLevel(0.0)
+{
+}
+
+void LightHandler::Begin()
+{
+    i2cmux::tcaselect(cLight);
+
+    if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE, 0x23, &Wire1))
+    {
+        Serial.println("Light sensor initialized");
+    }
+    else
+    {
+        Serial.println("Error initializing light sensor");
+    }
+}
+
+void LightHandler::Update()
+{
+    i2cmux::tcaselect(cLight);
+    lightLevel = lightMeter.readLightLevel();
+}
+
+void LightHandler::PrintInfo(Print &output, bool printConfig) const
+{
+    output.println("Light Sensor Info:");
+    output.print("Light Level: ");
+    output.print(lightLevel);
+    output.println(" lx");
+
+    if (printConfig)
+    {
+        output.println("Configuration:");
+        output.println("Mode: Continuous High Resolution");
+    }
+}
+
+float LightHandler::GetLightLevel() const
+{
+    return lightLevel;
+}
+
+Print &operator<<(Print &output, const LightHandler &handler)
+{
+    handler.PrintInfo(output);
+    return output;
+}
---
+++ src/handler/LightHandler.h
@@ -0,0 +1,26 @@
+#ifndef LIGHTHANDLER_H
+#define LIGHTHANDLER_H
+
+#include <Wire.h>
+#include <BH1750.h>
+#include "i2cmux.h"
+
+class LightHandler
+{
+public:
+    LightHandler(int cLight);
+    void Begin();
+    void Update();
+    void PrintInfo(Print &output, bool printConfig = false) const;
+    float GetLightLevel() const;
+
+private:
+    int cLight;
+    BH1750 lightMeter;
+    float lightLevel;
+};
+
+// Overload the << operator for Print class
+Print &operator<<(Print &output, const LightHandler &handler);
+
+#endif // LIGHTHANDLER_H
